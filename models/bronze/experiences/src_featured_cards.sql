aba62272d0546814caace5d456fc72caf8584c99
Zameel Shaji <zameelshaji01@gmail.com>
2025-11-11 19:58:15 +0000

    turned the categories into boolean values

--- models/bronze/experiences/src_featured_cards.sql
+++ models/bronze/experiences/src_featured_cards.sql
@@ -27,16 +27,16 @@ select
     updated_at,
     
     -- Check each category individually
-    CASE WHEN description ILIKE ANY (ARRAY['% hike %','% trail %','% climb %','% climbing %','% boulder %','% go ape %','% zip %','% outdoor %','% park %','% adventure %','% cycling%','% bike %','% kayak %','% canoe %','% surf %','% rafting %','% go-kart %','% paintball %','% farm %','% zoo %','% wildlife %','% treetop %']) THEN 'true' ELSE 'false' END as is_adventure,
+    CASE WHEN description ILIKE ANY (ARRAY['% hike %','% trail %','% climb %','% climbing %','% boulder %','% go ape %','% zip %','% outdoor %','% park %','% adventure %','% cycling%','% bike %','% kayak %','% canoe %','% surf %','% rafting %','% go-kart %','% paintball %','% farm %','% zoo %','% wildlife %','% treetop %']) THEN true ELSE false END as is_adventure,
     
-    CASE WHEN description ILIKE ANY (ARRAY['% museum %','% gallery %','% art %','% historic %','% heritage %','% landmark %','% monument%','% temple %','% cathedral %','% church %','% festival %','% exhibition %','% concert hall %','% opera %','% cultural %']) THEN 'true' ELSE 'false' END as is_culture,
+    CASE WHEN description ILIKE ANY (ARRAY['% museum %','% gallery %','% art %','% historic %','% heritage %','% landmark %','% monument%','% temple %','% cathedral %','% church %','% festival %','% exhibition %','% concert hall %','% opera %','% cultural %']) THEN true ELSE false END as is_culture,
     
-    CASE WHEN description ILIKE ANY (ARRAY['% restauran t%','% eatery %','% brunch %','% breakfast %','% lunch %','% dinner%','% cafe %','% café %','% coffee %','% bakery %','% doughnut %','% patisserie %','% pizza %','% burger %','% sushi %','% bistro %','% food %','% fine dining %']) THEN 'true' ELSE 'false' END as is_dining,
+    CASE WHEN description ILIKE ANY (ARRAY['% restauran t%','% eatery %','% brunch %','% breakfast %','% lunch %','% dinner%','% cafe %','% café %','% coffee %','% bakery %','% doughnut %','% patisserie %','% pizza %','% burger %','% sushi %','% bistro %','% food %','% fine dining %']) THEN true ELSE false END as is_dining,
     
-    CASE WHEN description ILIKE ANY (ARRAY['% cinema %','% movie %','%bowling%','% arcade %','% karaoke %','% live music%','% comedy %','% jazz %','% club %','% theatre %','% theater%','% show %','% performance %','% event %','% party %','% nightclub %','% escape room %']) THEN 'true' ELSE 'false' END as is_entertainment,
+    CASE WHEN description ILIKE ANY (ARRAY['% cinema %','% movie %','%bowling%','% arcade %','% karaoke %','% live music%','% comedy %','% jazz %','% club %','% theatre %','% theater%','% show %','% performance %','% event %','% party %','% nightclub %','% escape room %']) THEN true ELSE false END as is_entertainment,
     
-    CASE WHEN description ILIKE ANY (ARRAY['% gym%','% yoga %','% wellness%','% spa %','% pilates%','% fitness %','% clinic %','% massage %','% meditation %','% sauna %','% therapy %','% rehab %','% holistic %']) THEN 'true' ELSE 'false' END as is_health,
+    CASE WHEN description ILIKE ANY (ARRAY['% gym%','% yoga %','% wellness%','% spa %','% pilates%','% fitness %','% clinic %','% massage %','% meditation %','% sauna %','% therapy %','% rehab %','% holistic %']) THEN true ELSE false END as is_health,
     
-    CASE WHEN description ILIKE ANY (ARRAY['% bar %','% pub %','% brewery %','% taproom %','% wine %','% cocktail %','% speakeasy %','% gin %','% beer %','% drinks %','% whisky %','% rum %','% vodka %','% tasting %']) THEN 'true' ELSE 'false' END as is_drinks 
+    CASE WHEN description ILIKE ANY (ARRAY['% bar %','% pub %','% brewery %','% taproom %','% wine %','% cocktail %','% speakeasy %','% gin %','% beer %','% drinks %','% whisky %','% rum %','% vodka %','% tasting %']) THEN true ELSE false END as is_drinks 
 
 from {{ source("public", "featured_cards") }}
